439ae21d784b5a238ee3ded5d63262dafc81bdfd
Mihail Bodrov <connormiha1@gmail.com>
2016-10-22 17:24:08 +0300

    fix unit tests

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,8 +1,5 @@
-import * as bem from 'bem-cn';
 import * as React from 'react';
 import * as immutable from 'seamless-immutable';
 
-global.DEBUG = true;
 global.React = React;
 global.immutable = immutable;
-global.bem = bem;
